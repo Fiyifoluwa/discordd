cc9e7cc8d8544984e1df43d357976ae23edf658a
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-03-01 20:48:22 +0100

    init expo discord
    Signed-off-by: Fiyifoluwa <fiyifoluwa@outlook.com>

--- App.tsx
+++ App.tsx
@@ -1,20 +1,82 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
+
+import { StreamChat } from 'stream-chat';
+import { OverlayProvider, Chat, ChannelList, Channel, MessageList, MessageInput } from 'stream-chat-expo';
+import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+
+const API_KEY = 'k5dt9vpgquyu';
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [isReady, setIsReady] = useState(false);
+  const [selectedChannel, setSelectedChannel] = useState(null);
 
-  if (!isLoadingComplete) {
+  const connectUser = async () => {
+    try {
+      const user = await client.connectUser(
+        {
+          id: 'fiyi',
+          name: 'Fiyi',
+          image: 'https://randomuser.me/api/portraits/men/80.jpg',
+        },
+        client.devToken('fiyi')
+      );
+      setIsReady(true);
+      // console.log('User => ', user);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // const channel = client.channel('team', 'general', { name: 'General' });
+
+    // await channel.create();
+  };
+
+  useEffect(() => {
+    connectUser();
+  }, []);
+
+  const onChannelSelect = (channel: any) => {
+    setSelectedChannel(channel);
+  };
+
+  if (!isLoadingComplete || !isReady) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider overlayOpacity={{ value: 1 }}>
+          <Chat client={client}>
+            {!selectedChannel ? (
+              <ChannelList onSelect={onChannelSelect} />
+            ) : (
+              <>
+                <Channel channel={selectedChannel}>
+                  <Text
+                    style={{
+                      marginTop: 70,
+                      paddingHorizontal: 20,
+                    }}
+                    onPress={() => setSelectedChannel(null)}
+                  >
+                    go back
+                  </Text>
+                  <MessageList />
+                  <MessageInput />
+                </Channel>
+              </>
+            )}
+          </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -12,18 +12,15 @@ import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavigationContainer linking={LinkingConfiguration} theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -40,9 +37,6 @@ function RootNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group>
     </Stack.Navigator>
   );
 }
@@ -61,7 +55,8 @@ function BottomTabNavigator() {
       initialRouteName="TabOne"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
+      }}
+    >
       <BottomTab.Screen
         name="TabOne"
         component={TabOneScreen}
@@ -73,13 +68,9 @@ function BottomTabNavigator() {
               onPress={() => navigation.navigate('Modal')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
+              })}
+            >
+              <FontAwesome name="info-circle" size={25} color={Colors[colorScheme].text} style={{ marginRight: 15 }} />
             </Pressable>
           ),
         })}
@@ -99,9 +90,6 @@ function BottomTabNavigator() {
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
